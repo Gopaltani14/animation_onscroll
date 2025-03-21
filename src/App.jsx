555cfe95542fe99b388cb59ee20f467a6a48de2d
gopaltani14 <gopaltani14@gmail.com>
2025-03-21 09:15:18 +0000

    re updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,33 @@
+
 import './App.css'
 
 function App() {
 
   return (
-    <div>
-      {/* <!-- <section className="basic-grid">
-      <div className="card">1</div>
-      <div className="card">2</div>
-      <div className="card">3</div>
-      <div className="card">4</div>
-      <div className="card">5</div>
-      <div className="card">6</div>
-      <div className="card">7</div>
-      <div className="card">8</div>
-      <div className="card">9</div>
-      <div className="card">10</div>
-      <div className="card">11</div>
-      <div className="card">12</div>
-    </section> --> */}
-    <section className="photo-grid">
-      <div
-        className="card"
-      >
-        1
+    <>
+      <div className='view'>
+        <div className="block">1</div>
+        <div className="block">2</div>
+        <div className="block">3</div>
+        <div className="block">4</div>
+        <div className="block">5</div>
+        <div className="block">6</div>
+        <div className="block">7</div>
+        <div className="block">8</div>
+        <div className="block">9</div>
+        <div className="block">10</div>
+        <div className="block">1</div>
+        <div className="block">2</div>
+        <div className="block">3</div>
+        <div className="block">4</div>
+        <div className="block">5</div>
+        <div className="block">6</div>
+        <div className="block">7</div>
+        <div className="block">8</div>
+        <div className="block">9</div>
+        <div className="block">10</div>
       </div>
-      <div
-        className="card card-tall"
-      >
-        2
-      </div>
-      <div
-        className="card"
-      >
-        3
-      </div>
-      <div
-        className="card"
-      >
-        4
-      </div>
-      <div
-        className="card"
-      >
-        5
-      </div>
-      <div
-        className="card"
-      >
-        6
-      </div>
-      <div
-        className="card"
-      >
-        7
-      </div>
-      <div
-        className="card"
-      >
-        8
-      </div>
-      <div
-        className="card"
-      >
-        9
-      </div>
-      <div
-        className="card"
-      >
-        10
-      </div>
-      <div
-        className="card card-tall"
-      >
-        11
-      </div>
-      <div
-        className="card"
-        style={{
-          backgroundImage: "url('https://source.unsplash.com/64IhhrGg2IQ')"}}
-      >
-        12
-      </div>
-    </section>
-    </div>
+    </>
   )
 }
 
