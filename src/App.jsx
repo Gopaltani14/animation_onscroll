f5a77283dc2cffbb922d2e1dd402ee6f3410963e
gopaltani14 <gopaltani14@gmail.com>
2025-03-21 08:48:13 +0000

    animation on scroll

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='view'>
+        <div className="block">1</div>
+        <div className="block">2</div>
+        <div className="block">3</div>
+        <div className="block">4</div>
+        <div className="block">5</div>
+        <div className="block">6</div>
+        <div className="block">7</div>
+        <div className="block">8</div>
+        <div className="block">9</div>
+        <div className="block">10</div>
+        <div className="block">1</div>
+        <div className="block">2</div>
+        <div className="block">3</div>
+        <div className="block">4</div>
+        <div className="block">5</div>
+        <div className="block">6</div>
+        <div className="block">7</div>
+        <div className="block">8</div>
+        <div className="block">9</div>
+        <div className="block">10</div>
+      </div>
+    </>
+  )
+}
+
+export default App
